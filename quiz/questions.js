8bff7e23aec46b41df552813b54598dc7df35829
prince1901 <87210878+prince1901@users.noreply.github.com>
2021-07-11 03:36:10 +0530

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When a plot is sold for Rs. 18,700, the owner loses 15%. At what price must that plot be sold in order to gain 15%?",
+    answer: "Rs. 21,000	",
+    options: [
+      "Rs. 21,000	",
+      "Rs. 22,500",
+      "Rs. 22,600",
+      "Rs. 42,500"
+    ]
+  },
+    {
+    numb: 2,
+    question: "	If the simple interest on a sum of money for 2 years at 5% per annum is Rs. 50, what is the compound interest on the same at the same rate and for the same time?",
+    answer: "Rs. 51.25 ",
+    options: [
+      "Rs. 51.25 ",
+      "Rs. 52.25 ",
+      "Rs. 53.25 ",
+      "Rs. 54.75 "
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which feature of the OOPS gives the concept of reusability?",
+    answer: "Inheritance",
+    options: [
+      "Abstraction",
+      "Encapsulation",
+      "Inheritance",
+      "None Of The Obove"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which type of data file is analogous to an audio cassette tape?",
+    answer: "Sequential access file",
+    options: [
+      "Random access file",
+      "Sequential access file",
+      "Binary file",
+      "Source code file"
+    ]
+  },
+    {
+    numb: 5,
+    question: "	A sum fetched a total simple interest of Rs. 4016.25 at the rate of 9 p.c.p.a. in 5 years. What is the sum?",
+    answer: "8925",
+    options: [
+      "none",
+      "8925",
+      "8937",
+      "942"
+    ]
+  },
+
+];
